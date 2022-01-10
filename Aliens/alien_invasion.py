1812affd98a592241208b69c55cd257ac3282deb
Roy <213150451@sjtu.edu.cn>
2022-01-10 20:35:16 +0800

    star ship added to the main screen. Settings done in file settings.py. Ship defined in file ship.py

--- Aliens/alien_invasion.py
+++ Aliens/alien_invasion.py
@@ -1,14 +1,19 @@
 import sys
 import pygame
+from settings import *
+from ship import Ship
 
 class AlienInvasion:
     """游戏资源管理+行为管理"""
     def __init__(self):
         """游戏初始化"""
         pygame.init()
+        self.settings=Setting()
         # 像素
-        self.screen = pygame.display.set_mode((1200,800))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+        self.bg_color=self.settings.bg_color
+        self.ship=Ship(self)
     
     def run_game(self):
         """主循环"""
@@ -20,6 +25,10 @@ class AlienInvasion:
 
             # 让最近绘制的屏幕可见
             pygame.display.flip()
+            self.screen.fill(self.bg_color)
+            self.ship.blitme()
+    
+        
 
 if __name__ == "__main__":
     # 创建游戏实例
