b189b1805ba37a142c5447157d283b739b8da659
Roy <213150451@sjtu.edu.cn>
2022-01-10 21:26:49 +0800

    achieve moving of the star ship

--- Aliens/ship.py
+++ Aliens/ship.py
@@ -9,6 +9,15 @@ class Ship:
         self.rect = self.image.get_rect()
 
         self.rect.midbottom = (600,800)
+
+        self.moving_right = False
+        self.moving_left = False
     
     def blitme(self):
         self.screen.blit(self.image,self.rect)
+
+    def update(self):
+        if self.moving_right == True:
+            self.rect.x += 1
+        elif self.moving_left == True:
+            self.rect.x -= 1
